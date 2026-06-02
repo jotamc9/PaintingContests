a4302e92a96f9548dcc74edbebc84e5af49d2f1c
jotamc9 <jesusmartincarmona9@gmail.com>
2026-06-02 22:27:49 +0200

    chore: new components for calendar-layout and basic structure

--- src/app/pages/calendar/components/calendar-layout/calendar-layout.ts
+++ src/app/pages/calendar/components/calendar-layout/calendar-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CalendarBody } from './components/calendar-body/calendar-body';
+import { CalendarHeader } from './components/calendar-header/calendar-header';
+import { CalendarFooter } from './components/calendar-footer/calendar-footer';
 
 @Component({
   selector: 'app-calendar-layout',
-  imports: [],
+  imports: [CalendarHeader, CalendarBody, CalendarFooter],
   templateUrl: './calendar-layout.html',
   styleUrl: './calendar-layout.scss',
 })
-export class CalendarLayout {
-
-}
+export class CalendarLayout {}
---
+++ src/app/pages/calendar/components/calendar-layout/components/calendar-body/calendar-body.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-body',
+  imports: [],
+  templateUrl: './calendar-body.html',
+  styleUrl: './calendar-body.scss',
+})
+export class CalendarBody {
+
+}

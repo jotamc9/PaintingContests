01dbacd0bc9ad0d28746cb13ece9c0aa6175d514
jotamc9 <jesusmartincarmona9@gmail.com>
2026-06-04 20:22:44 +0200

    chore: add supabaseUrl and supabaseKey to environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  supabaseUrl: 'https://asaiyctiamutfkeeevwt.supabase.co',
+  supabaseKey: 'sb_publishable_igCd5kCKxDTFHWnvFHe8oQ_TP5blH9D',
+};

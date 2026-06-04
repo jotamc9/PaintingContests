c39d3d79280a5919f80b148c647b0a867a94f676
jotamc9 <jesusmartincarmona9@gmail.com>
2026-06-04 21:17:55 +0200

    feat: create supabase.client

---
+++ src/app/core/supabase/supabase.client.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey,
+    );
+  }
+
+  async testDatabase() {
+    return await this.supabase.from('test_connection').select('*');
+  }
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NextContests } from './components/next-contests/next-contests';
+import { SupabaseService } from '../../core/supabase/supabase.client';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,12 @@ import { NextContests } from './components/next-contests/next-contests';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  constructor(private supabaseService: SupabaseService) {}
+
+  async ngOnInit() {
+    const result = await this.supabaseService.testDatabase();
+
+    console.log(result);
+  }
+}

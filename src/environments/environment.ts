5e9f8c9189d7b80c6d9a59d6fc84a2545b0a47c3
jotamc9 <jesusmartincarmona9@gmail.com>
2026-06-05 13:08:33 +0200

    chore: add production value to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
+  production: false,
+
   supabaseUrl: 'https://asaiyctiamutfkeeevwt.supabase.co',
   supabaseKey: 'sb_publishable_igCd5kCKxDTFHWnvFHe8oQ_TP5blH9D',
 };

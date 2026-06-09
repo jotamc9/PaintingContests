84fe40a2af05043148c53baa94bfb590fc00aa2d
jotamc9 <jesusmartincarmona9@gmail.com>
2026-06-09 09:36:35 +0200

    refactor: SupabaseClient without SSR

--- src/app/core/supabase/supabase.service.ts
+++ src/app/core/supabase/supabase.service.ts
@@ -1,25 +1,13 @@
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../../environments/environment';
-import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SupabaseService {
-  private readonly _client: SupabaseClient | null;
-
-  constructor(@Inject(PLATFORM_ID) platformId: object) {
-    this._client = isPlatformBrowser(platformId)
-      ? createClient(environment.supabaseUrl, environment.supabaseKey)
-      : null;
-  }
-
-  get client(): SupabaseClient {
-    if (!this._client) {
-      throw new Error('Supabase client is not available');
-    }
-
-    return this._client;
-  }
+  readonly client: SupabaseClient = createClient(
+    environment.supabaseUrl,
+    environment.supabaseKey,
+  );
 }
